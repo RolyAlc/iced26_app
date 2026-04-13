eeb59ee1ab1dff11163f2521cae3de74a02feffd
rolyalc <rolalcedu@gmail.com>
2026-04-14 00:31:42 +0200

    new:: fichero constans de assets globales

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,5 @@
+/// Clase que contiene las rutas de los assets de la aplicación.
+class Assets {
+  static const appData = 'assets/data/app_data.json';
+  static const logoIced26 = 'assets/data/brand/logo_iced26_new2-1-1024x628.png';
+}
