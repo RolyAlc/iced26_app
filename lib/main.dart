de91b986fb1c8ca897503c37571178fb2a009c9f
rolyalc <rolalcedu@gmail.com>
2026-04-14 02:46:41 +0200

    stye:: añadir debug de tamaño de los widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:iced26/app/app.dart';
 
 void main() {
+  debugPaintSizeEnabled = true; // Muestra los bordes de widgets para depurar
   runApp(const MyApp());
 }
