d6d1031e7cbc92dfaa0a4cc637a8ae52e3cbb8a2
rolyalc <rolalcedu@gmail.com>
2026-04-18 22:47:14 +0200

    mod:: chore:: config: Configurar ProviderScope y los archivos generados por gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:iced26/app/app.dart';
-import 'package:iced26/core/data/app_data_repository.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: Quitar en producción
   debugPaintSizeEnabled = false; // Muestra los bordes de widgets para depurar
-  final repository = AppDataRepository();
-  runApp(MyApp(repository: repository));
+
+  runApp(const ProviderScope(child: MyApp()));
 }
