9db26ec10f6b31c639c34123e8fac10d037b45f7
rolyalc <rolalcedu@gmail.com>
2026-04-18 22:26:41 +0200

    refact:: domain: Completar la entidad SocialActivity y agregar AppData.empty factory

--- lib/domain/entities/social_activity.dart
+++ lib/domain/entities/social_activity.dart
@@ -1,6 +1,22 @@
-/// Entidad social actividad que representa una actividad social de la conferencia.
+import 'package:iced26/domain/entities/i18n_str.dart';
+
+/// Entidad que representa una actividad social de la conferencia.
 class SocialActivity {
   final String id;
+  final I18nStr title;
+  final I18nStr description;
+  final String date;
+  final String time;
+  final I18nStr location;
+  final String imgUrl;
 
-  SocialActivity({required this.id});
+  SocialActivity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.time,
+    required this.location,
+    required this.imgUrl,
+  });
 }
