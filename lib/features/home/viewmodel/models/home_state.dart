a1a497c06b35fca6cebcc5c7c6ad396b80c9d942
rolyalc <rolalcedu@gmail.com>
2026-04-18 22:48:05 +0200

    chore:: modelo de presentación de home añadido lista de social activity

--- lib/features/home/viewmodel/models/home_state.dart
+++ lib/features/home/viewmodel/models/home_state.dart
@@ -1,6 +1,7 @@
 import 'package:iced26/domain/entities/day.dart';
 import 'package:iced26/domain/entities/event_ui_model.dart';
 import 'package:iced26/domain/entities/new.dart';
+import 'package:iced26/domain/entities/social_activity.dart';
 import 'package:iced26/features/home/viewmodel/models/category_layout.dart';
 
 /// Modelo de presentación de la home.
@@ -9,6 +10,7 @@ class HomeState {
   final List<EventUIModel> featuredEvents;
   final CategoryLayout? categoryLayout;
   final List<NewsItem> news;
+  final List<SocialActivity> socialActivities;
   final String headerInfoLabel;
 
   HomeState({
@@ -16,6 +18,7 @@ class HomeState {
     required this.featuredEvents,
     this.categoryLayout,
     required this.news,
+    required this.socialActivities,
     required this.headerInfoLabel,
   });
 }
