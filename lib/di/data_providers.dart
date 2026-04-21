c76907e031984f691614131c256bb5e9943c0ed7
rolyalc <rolalcedu@gmail.com>
2026-04-21 14:43:06 +0200

    new:: nuevos proveedores de useCase

--- lib/di/data_providers.dart
+++ lib/di/data_providers.dart
@@ -1,21 +1,23 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import 'package:iced26/domain/repositories/agenda_repository.dart';
+import 'package:iced26/domain/repositories/schedule_repository.dart';
 import 'package:iced26/domain/repositories/home_repository.dart';
 import 'package:iced26/domain/repositories/config_repository.dart';
-import 'package:iced26/data/repositories/agenda_repository_impl.dart';
+import 'package:iced26/domain/repositories/favorites_repository.dart';
+import 'package:iced26/data/repositories/schedule_repository_impl.dart';
 import 'package:iced26/data/repositories/home_repository_impl.dart';
 import 'package:iced26/data/repositories/config_repository_impl.dart';
+import 'package:iced26/data/repositories/favorites_repository_impl.dart';
 import 'package:iced26/data/sources/local/json/local_json_service.dart';
 import 'package:iced26/di/core_providers.dart';
 
 part 'data_providers.g.dart';
 
-/// Provee el repositorio de la Agenda.
+/// Provee el repositorio de la Schedule.
 @riverpod
-AgendaRepository agendaRepository(AgendaRepositoryRef ref) {
+ScheduleRepository scheduleRepository(ScheduleRepositoryRef ref) {
   final db = ref.watch(appDatabaseProvider);
-  return AgendaRepositoryImpl(db);
+  return ScheduleRepositoryImpl(db);
 }
 
 /// Provee el repositorio de la Home.
@@ -31,4 +33,11 @@ ConfigRepository configRepository(ConfigRepositoryRef ref) {
   final db = ref.watch(appDatabaseProvider);
   final jsonService = LocalJsonService();
   return ConfigRepositoryImpl(db, jsonService);
+}
+
+/// Provee el repositorio de Favoritos.
+@riverpod
+FavoritesRepository favoritesRepository(FavoritesRepositoryRef ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return FavoritesRepositoryImpl(db);
 }
--- lib/di/domain_providers.dart
+++ lib/di/domain_providers.dart
@@ -2,21 +2,35 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'package:iced26/di/data_providers.dart';
 import 'package:iced26/domain/usecases/get_home_data_use_case.dart';
-import 'package:iced26/domain/usecases/get_agenda_data_use_case.dart';
+import 'package:iced26/domain/usecases/get_schedule_data_use_case.dart';
+import 'package:iced26/domain/usecases/watch_favorites_use_case.dart';
+import 'package:iced26/domain/usecases/toggle_favorite_use_case.dart';
 
 part 'domain_providers.g.dart';
 
 /// Provee el caso de uso para obtener los datos de la Home.
 @riverpod
 GetHomeDataUseCase getHomeDataUseCase(GetHomeDataUseCaseRef ref) {
-  final agendaRepo = ref.watch(agendaRepositoryProvider);
+  final scheduleRepo = ref.watch(scheduleRepositoryProvider);
   final homeRepo = ref.watch(homeRepositoryProvider);
-  return GetHomeDataUseCase(agendaRepo, homeRepo);
+  return GetHomeDataUseCase(scheduleRepo, homeRepo);
 }
 
-/// Provee el caso de uso para obtener los datos de la Agenda.
+/// Provee el caso de uso para obtener los datos de Schedule.
 @riverpod
-GetAgendaDataUseCase getAgendaDataUseCase(GetAgendaDataUseCaseRef ref) {
-  final agendaRepo = ref.watch(agendaRepositoryProvider);
-  return GetAgendaDataUseCase(agendaRepo);
+GetScheduleDataUseCase getScheduleDataUseCase(GetscheduleDataUseCaseRef ref) {
+  final scheduleRepo = ref.watch(scheduleRepositoryProvider);
+  return GetScheduleDataUseCase(scheduleRepo);
+}
+
+/// Provee el caso de uso para observar favoritos en tiempo real.
+@riverpod
+WatchFavoritesUseCase watchFavoritesUseCase(WatchFavoritesUseCaseRef ref) {
+  return WatchFavoritesUseCase(ref.watch(favoritesRepositoryProvider));
+}
+
+/// Provee el caso de uso para alternar un favorito.
+@riverpod
+ToggleFavoriteUseCase toggleFavoriteUseCase(ToggleFavoriteUseCaseRef ref) {
+  return ToggleFavoriteUseCase(ref.watch(favoritesRepositoryProvider));
 }
