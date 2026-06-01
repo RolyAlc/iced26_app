16fd7e156f2ed8c8c4e1d7800e7fb17eb5fe9a1b
RolyAlc <rolalcedu@gmail.com>
2026-06-01 13:12:59 +0200

    chore:: bloquear rotación pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:iced26/core/config/app_config.dart';
 import 'package:iced26/presentation/app/app.dart';
 
 /// Punto de entrada de la aplicación.
-void main() {
-  WidgetsFlutterBinding.ensureInitialized(); // Inicializa el motor de Flutter.
-  AppConfig.applyDebugFlags(); // Aplica flags de depuración.
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppConfig.applyDebugFlags();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(const ProviderScope(child: MyApp()));
 }
